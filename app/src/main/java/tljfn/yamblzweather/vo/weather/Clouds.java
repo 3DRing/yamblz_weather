269de9ccb5395f54ccfa5305e6e12f0b800d922a
3DRing <45933861qw>
2017-07-28 18:34:52 +0500

    adding custom equal method to WeatherMap in order to be able to test it

--- app/src/main/java/tljfn/yamblzweather/vo/weather/Clouds.java
+++ app/src/main/java/tljfn/yamblzweather/vo/weather/Clouds.java
@@ -10,4 +10,20 @@ public class Clouds {
     public Clouds(String all) {
         this.all = all;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Clouds clouds = (Clouds) o;
+
+        return all.equals(clouds.all);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return all.hashCode();
+    }
 }
