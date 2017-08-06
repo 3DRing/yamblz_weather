a00574df23912e4379add4669c525a6e8f34f10b
3DRing <45933861qw>
2017-08-07 00:13:47 +0500

    adding tests to raw-db converting

---
+++ app/src/test/java/tljfn/yamblzweather/model/db/DBConverterTest.java
@@ -0,0 +1,54 @@
+package tljfn.yamblzweather.model.db;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tljfn.yamblzweather.data.DataProvider;
+import tljfn.yamblzweather.model.api.data.weather.RawWeather;
+import tljfn.yamblzweather.model.db.weather.DBConverter;
+import tljfn.yamblzweather.model.db.weather.DBWeatherData;
+import tljfn.yamblzweather.model.errors.RawToDBConvertingException;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.fail;
+
+/**
+ * Created by ringov on 06.08.17.
+ */
+
+public class DBConverterTest {
+
+    DataProvider dataProvider;
+
+    @Before
+    public void setup() {
+        dataProvider = new DataProvider();
+    }
+
+    @Test
+    public void converting_to_db_data_correctly() {
+        RawWeather rawData = dataProvider.getNewYorkWeather();
+        DBWeatherData dbData = DBConverter.fromRawWeatherData(rawData);
+        long crtTime = System.currentTimeMillis();
+
+        // not used so far
+        //assertTrue(dbData.getId() == 5128581);
+
+        assertTrue(dbData.getCity().equals("New York"));
+        assertTrue(dbData.getCondition() == 721);
+        assertTrue(Double.compare(dbData.getTemperature(), 22.22) == 0);
+        assertTrue(equalTime(dbData.getTime(), crtTime));
+    }
+
+    @Test(expected = RawToDBConvertingException.class)
+    public void converting_to_db_data_error() {
+        RawWeather data = dataProvider.getBadWeather();
+        DBWeatherData dbWeatherData = DBConverter.fromRawWeatherData(data);
+        fail();
+    }
+
+    private boolean equalTime(long time1, long time2) {
+        return Math.abs(time1 - time2) < 1000; // diff less then 1 second
+    }
+}
+
