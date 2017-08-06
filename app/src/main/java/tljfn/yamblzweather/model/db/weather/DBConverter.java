707f4d0796496070490f83425da1b3b4eb8e26a2
3DRing <45933861qw>
2017-08-07 00:18:44 +0500

    adding converter for db-ui weather, removing unnecessary method

--- app/src/main/java/tljfn/yamblzweather/model/db/weather/DBConverter.java
+++ app/src/main/java/tljfn/yamblzweather/model/db/weather/DBConverter.java
@@ -25,31 +25,6 @@ public class DBConverter {
         return kelvins - KELVIN_OFFSET;
     }
 
-    private static WeatherCondition weatherIdToCondition(int id) {
-        // todo rewrite it in a more clear way
-        if (id >= 200 && id < 300) {
-            return WeatherCondition.Thunderstorm;
-        } else if (id >= 300 && id < 600) {
-            return WeatherCondition.Rainy;
-        } else if (id >= 600 && id < 700) {
-            return WeatherCondition.Snow;
-        } else if (id >= 700 && id < 800) {
-            return WeatherCondition.Atmospherically;
-        } else if (id == 800) {
-            return WeatherCondition.Clear;
-        } else if (id > 800 && id < 900) {
-            return WeatherCondition.Cloudy;
-        } else if (id >= 900 && id < 950) {
-            return WeatherCondition.Extreme;
-        } else if (id >= 957 && id <= 962) {
-            return WeatherCondition.Windy;
-        } else if (id >= 951 && id <= 956) {
-            return WeatherCondition.Calm;
-        } else {
-            return WeatherCondition.Other;
-        }
-    }
-
     public static DBWeatherData fromRawWeatherData(RawWeather weather) {
         List<Weather> weathers = weather.getWeather();
         String city = weather.getName();
@@ -74,16 +49,6 @@ public class DBConverter {
                 .temperature(temperature)
                 .time(System.currentTimeMillis())
                 .condition(condition)
-                .build();
-        return data;
-    }
-
-    public static UIWeatherData toUIWeatherData(DBWeatherData weather) {
-        UIWeatherData data = new UIWeatherData.Builder()
-                .city(weather.getCity())
-                .temperature(weather.getTemperature())
-                .time(weather.getTime())
-                .condition(weatherIdToCondition(weather.getCondition()))
                 .build();
         return data;
     }
