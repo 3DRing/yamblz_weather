e5d44906625178eb7450b95c5d9c812c60acc80a
3DRing <45933861qw>
2017-08-07 20:46:06 +0500

    adding view model module

---
+++ app/src/main/java/tljfn/yamblzweather/di/modules/ViewModelModule.java
@@ -0,0 +1,37 @@
+package tljfn.yamblzweather.di.modules;
+
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+import tljfn.yamblzweather.modules.about.AboutViewModel;
+import tljfn.yamblzweather.modules.base.viewmodel.ViewModelFactory;
+import tljfn.yamblzweather.modules.city.choose_city.ChooseCityViewModel;
+import tljfn.yamblzweather.modules.weather.WeatherViewModel;
+
+/**
+ * Created by ringov on 07.08.17.
+ */
+
+@Module
+public abstract class ViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(WeatherViewModel.class)
+    abstract ViewModel bindStartViewModel(WeatherViewModel startViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(AboutViewModel.class)
+    abstract ViewModel bindAboutViewModel(AboutViewModel aboutViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ChooseCityViewModel.class)
+    abstract ViewModel bindChooseCityViewModel(ChooseCityViewModel chooseCityViewModel);
+
+    @Binds
+    abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelFactory factory);
+}
